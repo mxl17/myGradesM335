0f495616a5c7d6f8dee075dc22b395fef701ce8f
mxl17 <89915581+mxl17@users.noreply.github.com>
2021-12-09 10:39:10 +0100

    + icon added & added FloatingActionButton

--- app/src/main/java/ch/zli/m335/mygrades/SubjectPage.java
+++ app/src/main/java/ch/zli/m335/mygrades/SubjectPage.java
@@ -1,2 +1,12 @@
-package ch.zli.m335.mygrades;public class SubjectPage {
+package ch.zli.m335.mygrades;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubjectPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
