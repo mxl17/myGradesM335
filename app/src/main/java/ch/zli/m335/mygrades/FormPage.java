b7caf580a9b3216c6c9420ce22cf17493ce81598
mxl17 <89915581+mxl17@users.noreply.github.com>
2021-12-09 16:02:15 +0100

    Ausgefülltes formular wird in DB geschrieben

--- app/src/main/java/ch/zli/m335/mygrades/FormPage.java
+++ app/src/main/java/ch/zli/m335/mygrades/FormPage.java
@@ -1,13 +1,43 @@
 package ch.zli.m335.mygrades;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import ch.zli.m335.mygrades.model.AppDatabase;
+import ch.zli.m335.mygrades.model.Grade;
+import ch.zli.m335.mygrades.model.GradeDao;
 
 public class FormPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_page);
+
+        Button submitForm = findViewById(R.id.submitForm);
+        Spinner subjectSpinner = findViewById(R.id.subjectSpinner);
+        EditText testTitle = findViewById(R.id.testTitle);
+        EditText gradeValue = findViewById(R.id.gradeValue);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "mygrades").allowMainThreadQueries().build();
+        GradeDao gradeDao = db.gradeDao();
+
+        submitForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Grade grade = new Grade();
+                grade.grade = Integer.parseInt(String.valueOf(gradeValue.getText()));
+                grade.subject = subjectSpinner.getSelectedItem().toString();
+                grade.testTitle = String.valueOf(testTitle.getText());
+
+                gradeDao.insertAll(grade);
+            }
+        });
     }
 }
