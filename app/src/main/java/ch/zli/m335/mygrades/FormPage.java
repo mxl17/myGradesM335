dba24c9e38510dc75e409b1a4fe5c816ec987f7e
mxl17 <89915581+mxl17@users.noreply.github.com>
2021-12-09 13:08:13 +0100

    FormPage Formular UI in form_page.xml erstellt

--- app/src/main/java/ch/zli/m335/mygrades/FormPage.java
+++ app/src/main/java/ch/zli/m335/mygrades/FormPage.java
@@ -1,2 +1,13 @@
-package ch.zli.m335.mygrades;public class FormPage {
+package ch.zli.m335.mygrades;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_page);
+    }
 }
