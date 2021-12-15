1cedb740b082d62eba08152111c94a6ea8653cd6
mxl17 <89915581+mxl17@users.noreply.github.com>
2021-12-15 09:33:13 +0100

    Wenn Noten vom ausgewählten Fach vorhanden sind, werden diese dynamisch angezeigt.

--- app/src/main/java/ch/zli/m335/mygrades/SubjectPage.java
+++ app/src/main/java/ch/zli/m335/mygrades/SubjectPage.java
@@ -2,15 +2,25 @@ package ch.zli.m335.mygrades;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
+import ch.zli.m335.mygrades.model.AppDatabase;
+import ch.zli.m335.mygrades.model.Grade;
+import ch.zli.m335.mygrades.model.GradeDao;
+
 public class SubjectPage extends AppCompatActivity {
     TextView subjectPageTitle;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +31,63 @@ public class SubjectPage extends AppCompatActivity {
 
         Intent intent = getIntent();
         subjectPageTitle = findViewById(R.id.subjectPageTitle);
+        linearLayout = findViewById(R.id.gradesLinearLayout);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "mygrades").allowMainThreadQueries().build();
+        GradeDao gradeDao = db.gradeDao();
+
+        List<Grade> allMathGrades = gradeDao.getAllMathGrades();
+        List<Grade> allEnglishGrades = gradeDao.getAllEnglishGrades();
+        List<Grade> allFrenchGrades = gradeDao.getAllFrenchGrades();
+        List<Grade> allChemistryGrades = gradeDao.getAllChemistryGrades();
+
+
 
         switch (intent.getStringExtra("title")) {
             case "Mathematik":
                 subjectPageTitle.setText("Noten Mathematik");
+
+                for (int i = 0; i < allMathGrades.size(); i++) {
+                    TextView testDetail = new TextView(this);
+                    testDetail.setText(allMathGrades.get(i).testTitle + "\t\t\t" + allMathGrades.get(i).grade);
+                    testDetail.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+                    testDetail.setPadding(0,20,0,0);
+                    linearLayout.addView(testDetail);
+                }
                 break;
 
             case "Englisch":
                 subjectPageTitle.setText("Noten Englisch");
+                for (int i = 0; i < allEnglishGrades.size(); i++) {
+                    TextView testDetail = new TextView(this);
+                    testDetail.setText(allEnglishGrades.get(i).testTitle + "\t\t\t" + allEnglishGrades.get(i).grade);
+                    testDetail.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+                    testDetail.setPadding(0,20,0,0);
+                    linearLayout.addView(testDetail);
+                }
                 break;
 
             case "Französisch":
                 subjectPageTitle.setText("Noten Französisch");
+                for (int i = 0; i < allFrenchGrades.size(); i++) {
+                    TextView testDetail = new TextView(this);
+                    testDetail.setText(allFrenchGrades.get(i).testTitle + "\t\t\t" + allFrenchGrades.get(i).grade);
+                    testDetail.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+                    testDetail.setPadding(0,20,0,0);
+                    linearLayout.addView(testDetail);
+                }
                 break;
 
             case "Chemie":
                 subjectPageTitle.setText("Noten Chemie");
+                for (int i = 0; i < allChemistryGrades.size(); i++) {
+                    TextView testDetail = new TextView(this);
+                    testDetail.setText(allChemistryGrades.get(i).testTitle + "\t\t\t" + allChemistryGrades.get(i).grade);
+                    testDetail.setTextSize(TypedValue.COMPLEX_UNIT_SP,24);
+                    testDetail.setPadding(0,20,0,0);
+                    linearLayout.addView(testDetail);
+                }
                 break;
         }
 
